85ee75513b1bf801f31924c0c6f0d8744c2fff74
brendaguerrero9 <00024317@uca.edu.sv>
2021-09-16 23:42:15 -0600

    Creando backend de registro de usuarios

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
     user: String,
     password: String,
     rol: String
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
+const User  = require('../models/user');
+const conn = require('./../DB/connection')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', (req, res, next) => {
+  var params = req.body;
+  var newUser = new User(params);
+
+  conn.connectDB().then(()=>{
+    
+    newUser
+    .save()
+    .then((usuario) => {
+      res.status(200).json({
+        "mensaje": "Usuario registrado correctamente.",
+        "Usuario": usuario,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
+  
 });
 
 module.exports = router;
