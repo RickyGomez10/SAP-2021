1a18acedbef8cd7c42b9b1f1dd41b6b558bb08af
dagr27 <00118717@uca.edu.sv>
2021-11-05 10:45:22 -0600

    Arreglando problema del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,19 @@
-$("#loginForm").on("submit", function (e) {
-    e.preventDefault();
-    $.ajax('/login/verificar', {
-        type: 'POST',  // http method
-        datatype: 'json',
-        async: true,
-        data: { user: document.getElementById('username').value, pass: document.getElementById('contra').value },  // data to submit
-        success: function (response) {
-            console.log(response);
-            location.href = '/perito'
-        },
-        error: function (response) {
-            console.log("error");
+document.querySelector("#loginForm").addEventListener('submit', e=>{
+    console.log("Buenas");
+    let loginData = {
+        username : document.forms.formLogin.username.value,
+        password : document.forms.formLogin.password.value,
+    }
+    fetch('/perito/verificar', {
+        method :'POST',
+        body: JSON.stringify(loginData),
+        headers:{
+            'Content-type' : 'application/json'
         }
-    });
-});
+    }).then(res => res)
+    .then( response =>{
+        console.log(response);
+    }).catch(err=>{
+        console.log(err);
+    })
+})
